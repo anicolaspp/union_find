7a23ed7c53e28bf9d60cfc338a134351a07245dc
Nicolas A Perez <anicolaspp@gmail.com>
2024-10-13 16:29:10 -0700

    graph components.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	uf := unionfind.New[int]()
 
 	for i := 0; i < 10; i++ {
-		uf.Union(i, i)
+		uf.Add(i)
 	}
 
 	fmt.Println(uf.Sets())
@@ -25,4 +25,25 @@ func main() {
 	}
 
 	fmt.Println(uf.Sets())
+
+	g := map[string][]string{
+		"A": []string{"B", "C"},
+		"C": []string{"D"},
+		"M": []string{"N", "X"},
+		"O": []string{"X"},
+	}
+
+	fmt.Println(graphComponents(g))
+}
+
+func graphComponents(g map[string][]string) [][]string {
+	uf := unionfind.New[string]()
+
+	for k, adj := range g {
+		for _, m := range adj {
+			uf.Union(k, m)
+		}
+	}
+
+	return uf.Sets()
 }
